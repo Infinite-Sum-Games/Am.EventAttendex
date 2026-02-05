1b231e13fc861f20609eb090c4a9d28527e137cc
Minoti <Minotigupta2006@gmail.com>
2026-02-05 21:49:57 +0530

    Initial trial

---
+++ src/routes/login/index.tsx
@@ -0,0 +1,14 @@
+import { LoginForm } from "@/components/login-form"
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/login/")({
+  component: LoginPage,
+})
+
+function LoginPage() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
