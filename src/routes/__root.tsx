ea7e0737e2874f490a3023d207e4e1be63fed8af
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-02-09 21:39:45 +0530

    feat: add Toaster component for notifications

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,21 @@
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router"
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
+import { TanStackDevtools } from "@tanstack/react-devtools"
+import { Toaster } from "@/components/ui/sonner"
 
-import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools"
 
-import type { QueryClient } from "@tanstack/react-query";
+import type { QueryClient } from "@tanstack/react-query"
 
 interface MyRouterContext {
-  queryClient: QueryClient;
+  queryClient: QueryClient
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
+      <Toaster position="top-center" />
       <TanStackDevtools
         config={{
           position: "bottom-right",
@@ -28,4 +30,4 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       />
     </>
   ),
-});
+})
